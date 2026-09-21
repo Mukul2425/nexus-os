dbdf918a953aeb407ae4977abe82cf24902202a4
Mukul Kumar <mukulyv2425@gmail.com>
2026-09-21 19:55:07 +0530

    Minor changes

--- backend/tests/unit/test_config.py
+++ backend/tests/unit/test_config.py
@@ -3,7 +3,7 @@ from app.core.config import settings
 
 def test_settings():
 
-    assert settings.VERSION == "0.9.0"
+    assert settings.VERSION == "1.0.0"
 
     assert settings.LLM_PROVIDER == "gemini"
 
